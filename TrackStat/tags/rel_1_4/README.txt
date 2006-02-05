0e31e575ca9f7d0292835d0c973e828dd2000168
nobody <nobody@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2006-02-05 16:51:26 +0000

    This commit was manufactured by cvs2svn to create tag 'rel_1_4'.

---
+++ TrackStat/tags/rel_1_4/README.txt
@@ -0,0 +1,80 @@
+1. LICENSE
+==========
+Copyright (C) 2006 Erland Isaksson (erland_i@hotmail.com)
+
+Portions of code derived from the iTunes plugin included in slimserver
+Copyright (C) 2001-2004 Sean Adams, Slim Devices Inc.
+
+Portions of code derived from the iTunesUpdate 1.5 plugin
+Copyright (c) 2004-2006 James Craig (james.craig@london.com)
+
+Portions of code derived from the SlimScrobbler plugin
+Copyright (c) 2004 Stewart Loving-Gibbard (sloving-gibbard@uswest.net)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+2. PREREQUISITES
+================
+- A slimserver 6.2.* installed and configured
+
+3. FILES
+========
+This archive should contain the following files:
+- readme.txt (this file)
+- license.txt (the license)
+- TrackStat/* (The application itself)
+
+4. INSTALLATION
+===============
+Unzip to the Plugins directory in the Slimserver
+installation.
+
+5. USAGE
+========
+This plugin for slimserver makes it possible to rate your songs and also handles statistic data about
+play counts and last played time. The ratings and statistic data is stored in a separate database table
+which will not be cleared during a complete rescan of the music library. Ratings and statistic data can also
+be imported from an iTunes Music Library xml-file. You can make backup/restore of your ratings and statistic data
+to a separate xml file to make it easier to backup the information to other storages. To be able to view the ratings
+when listening to music it is recomended to install the patched MusicInfoSCR plugin provided at http://erland.homeip.net/download.
+
+The play count and last playing time algoritm is based on the same logic in iTunes Update plugin, this means that play count
+will not be increased if you just listen on the first secondes of a track, you have to listen to most of the track to update
+play counts and last played time.
+
+To rate a track just press the numbers 1-5 on the remote, you will see a message on the Squeezebox display which confirms
+that the rating has been set.
+
+The TrackStat plugin will add a number of items to the MusicInfoSCR screensaver plugin which makes it possible to show
+the rating information for the current playing track directly on the screen on the Squeezebox. This requires a patched
+version of the MusicInfoSCR plugin provided at http://erland.homeip.net/download.
+
+The ratings, play count and last play time information will be stored in a track_statistics table, see sql files in SQL
+directory for exact layout of the table. If running slimserver under mysql, the database user configured for slimserver
+have privileges to create the table it will be created automatically at startup. If the user does not have privileges to 
+create the table it must be created manually with the SQL scripts provided in the SQL directory toghether with the plugin.
+
+The plugin supports import of ratings, play counts and last play time from iTunes. To do this you will have to configure
+a number of parameters for the TrackStat plugin in the plugins section of the slimserver settings web interface.
+
+You can take a backup of the ratings, play counts, last play time information to an xml-file using the buttons provided in 
+the plugins section of the slimserver settings web interface. This information can later be restored which makes this a suitable
+way to store backups of the information on an external media.
+
+At last you may also want to look at the SQLPlayList plugin provided at http://erland.homeip.net/download which makes it possible
+to create smart playlists using SQL queries. This can be used toghether with the TrackStat plugin to setup playlists such as:
+- All 4-5 rated songs
+- All 4-5 rated songs in genre pop or rock
+- All not rated songs
---
+++ TrackStat/tags/rel_1_4/src/Backup/File.pm
@@ -0,0 +1,351 @@
+#         TrackStat::Backup module
+#    Copyright (c) 2006 Erland Isaksson (erland_i@hotmail.com)
+# 
+#    Portions of code derived from the iTunes plugin included with slimserver
+#    SlimServer Copyright (C) 2001-2004 Sean Adams, Slim Devices Inc.
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+use strict;
+use warnings;
+                   
+package Plugins::TrackStat::Backup::File;
+
+use Date::Parse qw(str2time);
+use Fcntl ':flock'; # import LOCK_* constants
+use POSIX qw(strftime);
+use File::Spec::Functions qw(:ALL);
+use File::Basename;
+use XML::Parser;
+
+use Slim::Utils::Misc;
+
+
+my $backupParser;
+my $backupParserNB;
+my $backupFile;
+my $isScanning = 0;
+my $opened = 0;
+my $inTrack;
+my $inValue;
+my %item;
+my $currentKey;
+
+sub backupToFile
+{
+	my $filename = shift;
+
+	my $sql = ("SELECT url, playCount, lastPlayed, rating FROM track_statistics");
+
+	my $dbh = Slim::Music::Info::getCurrentDataStore()->dbh();
+	my $sth = $dbh->prepare( $sql );
+	$sth->execute();
+
+	debugMsg("Backup to: $filename\n");
+	my $output = FileHandle->new($filename, ">") or do {
+		warn "Could not open $filename for writing.";
+		return;
+	};
+	print $output '<?xml version="1.0" encoding="UTF-8"?>'."\n";
+	print $output "<TrackStat>\n";
+
+	my( $url, $playCount, $lastPlayed, $rating );
+	$sth->bind_columns( undef, \$url, \$playCount, \$lastPlayed, \$rating );
+	my $result;
+	while( $sth->fetch() ) {
+		if($url) {
+			$url = escape($url);
+			debugMsg("Backing up: $url\n");
+			print $output "	<track>\n		<url>$url</url>\n";
+			if($playCount) {
+				print $output "		<playCount>$playCount</playCount>\n";
+			}
+			if($lastPlayed) {
+				print $output "		<lastPlayed>$lastPlayed</lastPlayed>\n";
+			}
+			if($rating) {
+				print $output "		<rating>$rating</rating>\n";
+			}
+			print $output "	</track>\n";
+		}
+	}
+
+	print $output "</TrackStat>\n";
+	close $output;
+	$sth->finish();
+	msg("TrackStat:Backup: Backup completed at ".(strftime ("%Y-%m-%d %H:%M:%S",localtime()))."\n");
+}
+
+sub restoreFromFile
+{
+	$backupFile = shift;
+	debugMsg("Restore from: $backupFile\n");
+	$backupParser = XML::Parser->new(
+		'ErrorContext'     => 2,
+		'ProtocolEncoding' => 'UTF-8',
+		'NoExpand'         => 1,
+		'NoLWP'            => 1,
+		'Handlers'         => {
+
+			'Start' => \&handleStartElement,
+			'Char'  => \&handleCharElement,
+			'End'   => \&handleEndElement,
+		},
+	);
+
+	Slim::Utils::Scheduler::add_task(\&scanFunction);
+}
+
+sub stopScan {
+
+	if (stillScanning()) {
+
+		debugMsg("Was stillScanning - stopping old scan.\n");
+
+		Slim::Utils::Scheduler::remove_task(\&scanFunction);
+		$isScanning = 0;
+		$opened = 0;
+		
+		close(BACKUPFILE);
+		$backupParser = undef;
+		resetScanState();
+	}
+}
+
+sub stillScanning {
+	return $isScanning;
+}
+
+sub doneScanning {
+	debugMsg("done Scanning: unlocking and closing\n");
+
+	if (defined $backupParserNB) {
+
+		# This spews, but it's harmless.
+		eval { $backupParserNB->parse_done };
+	}
+
+	$backupParserNB = undef;
+	$backupParser   = undef;
+
+	$opened = 0;
+
+	$isScanning = 0;
+
+	# Don't leak filehandles.
+	close(BACKUPFILE);
+
+	msg("TrackStat:Backup: Restore completed at ".(strftime ("%Y-%m-%d %H:%M:%S",localtime()))."\n");
+
+	# Take the scanner off the scheduler.
+	Slim::Utils::Scheduler::remove_task(\&scanFunction);
+}
+
+sub resetScanState {
+
+	debugMsg("Resetting scan state.\n");
+
+	$inTrack = 0;
+	$inValue = 0;
+	%item = ();
+	$currentKey = undef;
+}
+
+sub scanFunction {
+	# this assumes that iTunes uses file locking when writing the xml file out.
+	if (!$opened) {
+		open(BACKUPFILE, $backupFile) || do {
+			warn "Couldn't open backup file: $backupFile";
+			return 0;
+		};
+
+		$opened = 1;
+
+		resetScanState();
+
+		if (defined $backupParser) {
+
+			debugMsg("Created a new Non-blocking XML parser.\n");
+
+			$backupParserNB = $backupParser->parse_start();
+
+		} else {
+
+			debugMsg("No backupParser was defined!\n");
+		}
+	}
+
+	# parse a little more from the stream.
+	if (defined $backupParserNB) {
+
+		debugMsg("Parsing next bit of XML..\n");
+
+		local $/ = '</track>';
+		my $line;
+
+		for (my $i = 0; $i < 25; $i++) {
+			$line .= <BACKUPFILE>;
+		}
+
+		$line =~ s/&#(\d*);/escape(chr($1))/ge;
+
+		$backupParserNB->parse_more($line);
+
+		return 1;
+	}
+
+	debugMsg("No backupParserNB defined!\n");
+
+	return 0;
+}
+
+sub handleStartElement {
+	my ($p, $element) = @_;
+
+	# Don't care about the outer <dict> right after <plist>
+	if ($inTrack) {
+		$currentKey = $element;
+		$inValue = 1;
+	}
+	if ($element eq 'track') {
+		$inTrack = 1;
+	}
+	
+
+}
+
+sub handleCharElement {
+	my ($p, $value) = @_;
+
+	if ($inValue && $currentKey) {
+		$item{$currentKey} = $value;
+	}
+}
+
+sub handleEndElement {
+	my ($p, $element) = @_;
+
+	$inValue = 0;
+	
+	# Done reading this entry - add it to the database.
+	if ($inTrack && $element eq 'track') {
+
+		$inTrack = 0;
+
+		$item{'url'} = unescape($item{'url'});
+		restoreTrack(\%item);
+
+		%item = ();
+	}
+
+	# Finish up
+	if ($element eq 'TrackStat') {
+		debugMsg("iTunes: Finished scanning iTunes XML\n");
+
+		doneScanning();
+
+		return 0;
+	}
+}
+
+sub restoreTrack 
+{
+	my $curTrack = shift;
+	
+	my $url       = $curTrack->{'url'};
+	my $playCount = $curTrack->{'playCount'};
+	my $lastPlayed = $curTrack->{'lastPlayed'};
+	my $rating   = $curTrack->{'rating'};
+	
+	my $ds        = Slim::Music::Info::getCurrentDataStore();
+	my $track     = $ds->objectForUrl($url);
+
+	return unless $track;
+
+	my $trackHandle = Plugins::TrackStat::Plugin::searchTrackInStorage($url);
+	my $sql;
+	
+	if ($playCount) {
+		debugMsg("Marking as played in storage: $playCount\n");
+		if($trackHandle) {
+			if($lastPlayed) {
+				$sql = ("UPDATE track_statistics set playCount=$playCount, lastPlayed=$lastPlayed where url='$url'");
+			}else {
+				$sql = ("UPDATE track_statistics set playCount=$playCount where url='$url'");
+			}
+		}else {
+			if($lastPlayed) {
+				$sql = ("INSERT INTO track_statistics (url,playCount,lastPlayed) values ('$url',$playCount,$lastPlayed)");
+			}else {
+				$sql = ("INSERT INTO track_statistics (url,playCount) values ('$url',$playCount)");
+			}
+		}
+		my $dbh = Slim::Music::Info::getCurrentDataStore()->dbh();
+		my $sth = $dbh->prepare( $sql );
+		
+		$sth->execute();
+		$dbh->commit();
+
+		$sth->finish();
+	}
+	
+	#Lookup again since the row can have been created above
+	$trackHandle = Plugins::TrackStat::Plugin::searchTrackInStorage( $url);
+	if ($rating && $rating ne "") {
+		debugMsg("Store rating: $rating\n");
+	    #ratings are 0-5 stars, 100 = 5 stars
+
+		if ($trackHandle) {
+			$sql = ("UPDATE track_statistics set rating=$rating where url='$url'");
+		} else {
+			$sql = ("INSERT INTO track_statistics (url,rating) values ('$url',$rating)");
+		}
+		my $dbh = Slim::Music::Info::getCurrentDataStore()->dbh();
+		my $sth = $dbh->prepare( $sql );
+		
+		$sth->execute();
+		$dbh->commit();
+
+		$sth->finish();
+	}
+}
+
+# A wrapper to allow us to uniformly turn on & off debug messages
+sub debugMsg
+{
+	my $message = join '','TrackStat:Backup: ',@_;
+	msg ($message) if (Slim::Utils::Prefs::get("plugin_trackstat_showmessages"));
+}
+
+# other people call us externally.
+*escape   = \&URI::Escape::uri_escape_utf8;
+
+# don't use the external one because it doesn't know about the difference
+# between a param and not...
+#*unescape = \&URI::Escape::unescape;
+sub unescape {
+        my $in      = shift;
+        my $isParam = shift;
+
+        $in =~ s/\+/ /g if $isParam;
+        $in =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
+
+        return $in;
+}
+
+1;
+
+__END__
