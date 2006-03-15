c760b86631f40e3ff6607e0990408dddc80064f9
erland <erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2006-03-15 07:43:25 +0000

    Corrected compilation error

--- TrackStat/trunk/src/Storage.pm
+++ TrackStat/trunk/src/Storage.pm
@@ -155,7 +155,7 @@ sub saveRating {
 	my $sth = $dbh->prepare( $sql );
 	eval {
 		$sth->bind_param(1, $searchString , SQL_VARCHAR);
-		if ($trackHandle || defined(mbId)) {
+		if ($trackHandle || defined($mbId)) {
 			$sth->bind_param(2, $url , SQL_VARCHAR);
 		}
 		$sth->execute();
