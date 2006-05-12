37dc3611bdcf98cf6d2e3fa2516d68c5cc34f01c
erland <erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2006-05-12 18:50:02 +0000

    Added added column, added track_history table, changed url from text to varchar

--- TrackStat/trunk/src/SQL/mysql/dbcreate.sql
+++ TrackStat/trunk/src/SQL/mysql/dbcreate.sql
@@ -1,9 +1,17 @@
 CREATE TABLE track_statistics (
-  url text NOT NULL,
+  url varchar(255) NOT NULL,
   musicbrainz_id varchar(40),	-- musicbrainz uuid (36 bytes of text)
   playCount int(10) unsigned,
+  added int(10) unsigned,
   lastPlayed int(10) unsigned,
   rating int(10) unsigned
 ) TYPE=InnoDB;
 
+CREATE TABLE track_history (
+  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+  url varchar(255) NOT NULL,
+  musicbrainz_id varchar(40),	-- musicbrainz uuid (36 bytes of text)
+  played int(10) unsigned,
+  rating int(10) unsigned
+) TYPE=InnoDB;
 
