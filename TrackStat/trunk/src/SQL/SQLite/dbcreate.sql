f2838823149baa2517ee5fea4c41e3bbef400aef
erland <erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2006-05-22 17:42:19 +0000

    Removed autoincrement keyword

--- TrackStat/trunk/src/SQL/SQLite/dbcreate.sql
+++ TrackStat/trunk/src/SQL/SQLite/dbcreate.sql
@@ -8,7 +8,7 @@ CREATE TABLE track_statistics (
 );
 
 CREATE TABLE track_history (
-  id integer primary key autoincrement,
+  id integer primary key,
   url varchar NOT NULL,
   musicbrainz_id varchar,  -- musicbrainz uuid (36 bytes of text)
   played integer,
--- TrackStat/trunk/src/SQL/SQLite/dbupgrade_history.sql
+++ TrackStat/trunk/src/SQL/SQLite/dbupgrade_history.sql
@@ -1,5 +1,5 @@
 CREATE TABLE track_history (
-  id integer primary key autoincrement,
+  id integer primary key,
   url varchar NOT NULL,
   musicbrainz_id varchar,  -- musicbrainz uuid (36 bytes of text)
   played integer,
