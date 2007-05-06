9f4a31887aec398b47bcdad247c8653ef082a66d
erland <erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2007-05-06 18:27:07 +0000

    Updated version

--- TrackStat/trunk/src/Plugin.pm
+++ TrackStat/trunk/src/Plugin.pm
@@ -61,7 +61,7 @@ use Plugins::TrackStat::Storage;
 use vars qw($VERSION);
 $VERSION = substr(q$Revision$,10);
 
-my $PLUGINVERSION = '1.36.1';
+my $PLUGINVERSION = '1.37';
 
 #################################################
 ### Global constants - do not change casually ###
