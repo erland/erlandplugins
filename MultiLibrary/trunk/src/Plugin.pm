b1cfe0c2fe58309d4accc84af5283d24ea4aef80
erland <erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2007-05-15 04:32:47 +0000

    Updated version

--- MultiLibrary/trunk/src/Plugin.pm
+++ MultiLibrary/trunk/src/Plugin.pm
@@ -43,7 +43,7 @@ my $sqlerrors = '';
 my $soapLiteError = 0;
 my $supportDownloadError = undef;
 my %currentLibrary = ();
-my $PLUGINVERSION = '1.3.1';
+my $PLUGINVERSION = '1.3.2';
 my $internalMenus = undef;
 my $customBrowseMenus = undef;
 
