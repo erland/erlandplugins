b5fa41c85e8fe407be257d3ba6f04fe557e2c98a
erland <erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2007-05-20 18:55:13 +0000

    Corrected problem where large configuration wasnt possible to save

--- SQLPlayList/trunk/src/Plugin.pm
+++ SQLPlayList/trunk/src/Plugin.pm
@@ -45,7 +45,7 @@ my $playLists = undef;
 my $playListTypes = undef;
 my $sqlerrors = '';
 my $soapLiteError = 0;
-my $PLUGINVERSION = '1.19';
+my $PLUGINVERSION = '1.19.1';
 
 my $configManager = undef;
 
