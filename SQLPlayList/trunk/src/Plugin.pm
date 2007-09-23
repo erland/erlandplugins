2cffbef52f82e1c01e7c51a1dcb0d17c876fe34b
erland <erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2007-09-23 17:13:49 +0000

    Updated version

--- SQLPlayList/trunk/src/Plugin.pm
+++ SQLPlayList/trunk/src/Plugin.pm
@@ -45,7 +45,7 @@ my $playLists = undef;
 my $playListTypes = undef;
 my $sqlerrors = '';
 my $soapLiteError = 0;
-my $PLUGINVERSION = '1.20.2';
+my $PLUGINVERSION = '1.21';
 
 my $configManager = undef;
 
