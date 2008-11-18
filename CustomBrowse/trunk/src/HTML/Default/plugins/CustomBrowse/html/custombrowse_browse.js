b1e56d28016f42c5210fb6c2766692ab1157d5f1
isaksson.erland <isaksson.erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2008-11-18 17:48:29 +0000

    Corrected so sort optionns are shown in default skin

--- CustomBrowse/trunk/src/HTML/Default/plugins/CustomBrowse/html/custombrowse_browse.js
+++ CustomBrowse/trunk/src/HTML/Default/plugins/CustomBrowse/html/custombrowse_browse.js
@@ -1,5 +1,4 @@
-CBBrowse = function(){
-	return {
+CBBrowse = {
 		init : function(){
 			var el;
 
@@ -12,39 +11,37 @@ CBBrowse = function(){
 
 			// Album view selector
 			if (Ext.get('viewSelect')) {
-				var viewMode = (Utils.getCookie('SqueezeCenter-albumView') && Utils.getCookie('SqueezeCenter-albumView').match(/[012]/) ? Utils.getCookie('SqueezeCenter-albumView') : '0');
+				var viewMode = (SqueezeJS.getCookie('SqueezeCenter-albumView') && SqueezeJS.getCookie('SqueezeCenter-albumView').match(/[012]/) ? SqueezeJS.getCookie('SqueezeCenter-albumView') : '0');
 				var menu;
 				if(artworkList==1) {
 					menu = new Ext.menu.Menu({
 						items: [
 							new Ext.menu.CheckItem({
-								text: strings['switch_to_list'],
+								text: SqueezeJS.string('switch_to_list'),
 								cls: 'albumList',
 								handler: function(){ CBBrowse.toggleGalleryView(2) },
 								group: 'viewMode',
 								checked: viewMode == 2
 							}),
 							new Ext.menu.CheckItem({
-								text: strings['switch_to_extended_list'],
+								text: SqueezeJS.string('switch_to_extended_list'),
 								cls: 'albumXList',
 								handler: function(){ CBBrowse.toggleGalleryView(0) },
 								group: 'viewMode',
 								checked: viewMode == 0
 							}),
 							new Ext.menu.CheckItem({
-								text: strings['switch_to_gallery'],
+								text: SqueezeJS.string('switch_to_gallery'),
 								cls: 'albumListGallery',
 								handler: function(){ CBBrowse.toggleGalleryView(1) },
 								group: 'viewMode',
 								checked: viewMode == 1
 							})
-						],
-						shadow: Ext.isGecko && Ext.isMac ? true : 'sides'
+						]
 					});
 				}else {
 					menu = new Ext.menu.Menu({
-						items: [],
-						shadow: Ext.isGecko && Ext.isMac ? true : 'sides'
+						items: []
 					});
 				}
 
@@ -52,10 +49,10 @@ CBBrowse = function(){
 				if (orderByList) {
 					menu.add(
 							'-',
-							'<span class="menu-title">' + strings['sort_by'] + '...</span>'
+							'<span class="menu-title">' + SqueezeJS.string('sort_by') + '...</span>'
 					);
 
-					var sortOrder = Utils.getCookie('SlimServer-CustomBrowse-option');
+					var sortOrder = SqueezeJS.getCookie('SlimServer-CustomBrowse-option');
 					for (order in orderByList) {
 						menu.add(new Ext.menu.CheckItem({
 							text: order,
@@ -69,7 +66,8 @@ CBBrowse = function(){
 				}
 
 
-				new Ext.SplitButton('viewSelect', {
+				new Ext.SplitButton({
+					renderTo: 'viewSelect', 
 					icon: webroot + 'html/images/albumlist' + viewMode  + '.gif',
 					cls: 'x-btn-icon',
 					menu: menu,
@@ -79,8 +77,8 @@ CBBrowse = function(){
 						}
 						this.fireEvent('arrowclick', this, ev);
 					},
-					tooltip: strings['display_options'],
-					arrowTooltip: strings['display_options'],
+					tooltip: SqueezeJS.string('display_options'),
+					arrowTooltip: SqueezeJS.string('display_options'),
 					tooltipType: 'title'
 				});
 			}
@@ -97,17 +95,17 @@ CBBrowse = function(){
 			params = params.replace(/&artwork=\w*/gi, '');
 
 			if (artwork == 1) {
-				Utils.setCookie( 'SqueezeCenter-albumView', "1" );
+				SqueezeJS.setCookie( 'SqueezeCenter-albumView', "1" );
 				params += '&artwork=1';
 			}
 
 			else if (artwork == 2) {
-				Utils.setCookie( 'SqueezeCenter-albumView', "2" );
+				SqueezeJS.setCookie( 'SqueezeCenter-albumView', "2" );
 				params += '&artwork=2';
 			}
 
 			else {
-				Utils.setCookie( 'SqueezeCenter-albumView', "" );
+				SqueezeJS.setCookie( 'SqueezeCenter-albumView', "" );
 				params += '&artwork=0';
 			}
 
@@ -121,12 +119,9 @@ CBBrowse = function(){
 			if (option)
 				params += '&option=' + option;
 
-			Utils.setCookie('SlimServer-CustomBrowse-option', option);
+			SqueezeJS.setCookie('SlimServer-CustomBrowse-option', option);
 			location.search = params;
 		}
 
-	};
-}();
-
-Ext.EventManager.onDocumentReady(CBBrowse.init, CBBrowse, true);
+};
 
