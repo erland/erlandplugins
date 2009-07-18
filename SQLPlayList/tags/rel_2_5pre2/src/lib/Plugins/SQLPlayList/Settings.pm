7b139bdb5e5e3f41c1757bb1ee5c07710394f9b8
isaksson.erland <isaksson.erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2009-07-18 09:33:27 +0000

    Creating release 2.5pre2

---
+++ SQLPlayList/tags/rel_2_5pre2/src/lib/Plugins/SQLPlayList/Settings.pm
@@ -0,0 +1,77 @@
+#    Copyright (c) 2007 Erland Isaksson (erland_i@hotmail.com)
+# 
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+package Plugins::SQLPlayList::Settings;
+
+use strict;
+use base qw(Plugins::SQLPlayList::BaseSettings);
+
+use File::Basename;
+use File::Next;
+
+use Slim::Utils::Log;
+use Slim::Utils::Prefs;
+use Slim::Utils::Misc;
+use Slim::Utils::Strings;
+
+my $prefs = preferences('plugin.sqlplaylist');
+my $log   = logger('plugin.sqlplaylist');
+
+my $plugin; # reference to main plugin
+
+sub new {
+	my $class = shift;
+	$plugin   = shift;
+
+	$class->SUPER::new($plugin,1);
+}
+
+sub name {
+	return 'PLUGIN_SQLPLAYLIST';
+}
+
+sub page {
+	return 'plugins/SQLPlayList/settings/basic.html';
+}
+
+sub currentPage {
+	return Slim::Utils::Strings::string('PLUGIN_SQLPLAYLIST_SETTINGS');
+}
+
+sub pages {
+	my %page = (
+		'name' => Slim::Utils::Strings::string('PLUGIN_SQLPLAYLIST_SETTINGS'),
+		'page' => page(),
+	);
+	my @pages = (\%page);
+	return \@pages;
+}
+
+sub prefs {
+        return ($prefs, qw(playlist_directory template_directory enable_web_mixerfunction));
+}
+
+sub handler {
+	my ($class, $client, $paramRef) = @_;
+
+	my $result = $class->SUPER::handler($client, $paramRef);
+	if ($paramRef->{'saveSettings'}) {
+		Plugins::SQLPlayList::Plugin::getConfigManager()->initWebAdminMethods();
+	}	
+	return $result;
+}
+
+		
+1;
