cffc6df2de72231c09785ea7bb954fe02245f653
isaksson.erland <isaksson.erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2009-07-18 06:21:27 +0000

    Created unique index names

--- TrackStat/trunk/src/SQL/SQLite/dbindex.sql
+++ TrackStat/trunk/src/SQL/SQLite/dbindex.sql
@@ -1,6 +1,8 @@
-CREATE INDEX IF NOT EXISTS persistentdb.urlIndex on track_statistics (url);
-CREATE INDEX IF NOT EXISTS persistentdb.musicbrainzIndex on track_statistics (musicbrainz_id);
-CREATE INDEX IF NOT EXISTS persistentdb.urlIndex on track_history (url);
-CREATE INDEX IF NOT EXISTS persistentdb.musicbrainzIndex on track_history (musicbrainz_id);
+DROP INDEX IF EXISTS persistentdb.urlIndex;
+DROP INDEX IF EXISTS persistentdb.musicbrainzIndex;
+CREATE INDEX IF NOT EXISTS persistentdb.tsurlIndex on track_statistics (url);
+CREATE INDEX IF NOT EXISTS persistentdb.tsmusicbrainzIndex on track_statistics (musicbrainz_id);
+CREATE INDEX IF NOT EXISTS persistentdb.tshurlIndex on track_history (url);
+CREATE INDEX IF NOT EXISTS persistentdb.tshmusicbrainzIndex on track_history (musicbrainz_id);
 CREATE INDEX IF NOT EXISTS trackStatMBIndex on tracks (musicbrainz_id)
 
