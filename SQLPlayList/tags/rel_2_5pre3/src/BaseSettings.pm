f80dd4b4657330a5656af0397e2f42442d3dd042
isaksson.erland <isaksson.erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2009-08-02 09:05:15 +0000

    Creating release 2.5pre3

---
+++ SQLPlayList/tags/rel_2_5pre3/src/BaseSettings.pm
@@ -0,0 +1,69 @@
+#    Copyright (c) 2007 Erland Isaksson (erland_i@hotmail.com)
+# 
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package Plugins::SQLPlayList::BaseSettings;
+
+use strict;
+use base qw(Slim::Web::Settings);
+
+use File::Basename;
+use File::Next;
+
+use Slim::Utils::Log;
+use Slim::Utils::Prefs;
+use Slim::Utils::Misc;
+
+my $prefs = preferences('plugin.sqlplaylist');
+my $log   = logger('plugin.sqlplaylist');
+
+my $plugin; # reference to main plugin
+my %subPages = ();
+
+sub new {
+	my $class = shift;
+	$plugin   = shift;
+	my $default = shift;
+
+	if(!defined($default) || !$default) {
+		if ($class->can('page') && $class->can('handler')) {
+			if(UNIVERSAL::can("Slim::Web::Pages","addPageFunction")) {
+				Slim::Web::Pages->addPageFunction($class->page, $class);
+			}else {
+				Slim::Web::HTTP::addPageFunction($class->page, $class);
+			}
+		}
+	}else {
+		$class->SUPER::new();
+	}
+	$subPages{$class->name()} = $class;
+}
+
+sub handler {
+	my ($class, $client, $params) = @_;
+
+	my %currentSubPages = ();
+	for my $key (keys %subPages) {
+		my $pages = $subPages{$key}->pages($client,$params);
+		for my $page (@$pages) {
+			$currentSubPages{$page->{'name'}} = $page->{'page'};
+		}
+	}
+	$params->{'subpages'} = \%currentSubPages;
+	$params->{'subpage'} = $class->currentPage($client,$params);
+	return $class->SUPER::handler($client, $params);
+}
+		
+1;
