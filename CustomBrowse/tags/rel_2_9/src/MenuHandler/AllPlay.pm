26af9c43cab35f90a2ff85d96ce242b362bb7baa
isaksson.erland <isaksson.erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2009-09-29 04:38:52 +0000

    Creating release 2.9

---
+++ CustomBrowse/tags/rel_2_9/src/MenuHandler/AllPlay.pm
@@ -0,0 +1,55 @@
+# 			MenuHandler::AllPlay module
+#
+#    Copyright (c) 2006 Erland Isaksson (erland_i@hotmail.com)
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package Plugins::CustomBrowse::MenuHandler::AllPlay;
+
+use strict;
+
+use base qw(Slim::Utils::Accessor);
+use Plugins::CustomBrowse::MenuHandler::BasePlay;
+our @ISA = qw(Plugins::CustomBrowse::MenuHandler::BasePlay);
+
+use File::Spec::Functions qw(:ALL);
+
+sub new {
+	my $class = shift;
+	my $parameters = shift;
+
+	my $self = $class->SUPER::new($parameters);
+
+	return $self;
+}
+
+sub getItems {
+	my $self = shift;
+	my $client = shift;
+	my $item = shift;
+	my $items = shift;
+
+	my @result = ();
+	if(defined($items)) {
+		@result = @$items;
+	}else {
+		push @result,$item;
+	}
+	return \@result;
+}
+
+1;
+
+__END__
