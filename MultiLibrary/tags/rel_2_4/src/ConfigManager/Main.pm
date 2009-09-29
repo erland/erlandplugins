a4e720b3d297f6f311ed604fdeaf3f052499183f
isaksson.erland <isaksson.erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2009-09-29 04:42:14 +0000

    Creating release 2.4

---
+++ MultiLibrary/tags/rel_2_4/src/ConfigManager/Main.pm
@@ -0,0 +1,462 @@
+# 			ConfigManager::Main module
+#
+#    Copyright (c) 2006 Erland Isaksson (erland_i@hotmail.com)
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package Plugins::MultiLibrary::ConfigManager::Main;
+
+use strict;
+
+use base qw(Slim::Utils::Accessor);
+
+use Plugins::MultiLibrary::ConfigManager::TemplateParser;
+use Plugins::MultiLibrary::ConfigManager::ContentParser;
+use Plugins::MultiLibrary::ConfigManager::TemplateContentParser;
+use Plugins::MultiLibrary::ConfigManager::PluginLoader;
+use Plugins::MultiLibrary::ConfigManager::DirectoryLoader;
+use Plugins::MultiLibrary::ConfigManager::ParameterHandler;
+use Plugins::MultiLibrary::ConfigManager::LibraryWebAdminMethods;
+use FindBin qw($Bin);
+use File::Spec::Functions qw(:ALL);
+use Slim::Utils::Prefs;
+
+__PACKAGE__->mk_accessor( rw => qw(logHandler pluginPrefs pluginId pluginVersion downloadApplicationId supportDownloadError contentDirectoryHandler templateContentDirectoryHandler templateDirectoryHandler templateDataDirectoryHandler contentPluginHandler templatePluginHandler parameterHandler templateParser contentParser templateContentParser webAdminMethods addSqlErrorCallback templates items downloadVersion) );
+
+my $prefs = preferences('plugin.multilibrary');
+
+sub new {
+	my $class = shift;
+	my $parameters = shift;
+
+	my $self = $class->SUPER::new();
+	$self->logHandler($parameters->{'logHandler'});
+	$self->pluginPrefs($parameters->{'pluginPrefs'});
+	$self->pluginId($parameters->{'pluginId'});
+	$self->pluginVersion($parameters->{'pluginVersion'});
+	$self->downloadApplicationId($parameters->{'downloadApplicationId'});
+	$self->supportDownloadError($parameters->{'supportDownloadError'});
+	$self->addSqlErrorCallback($parameters->{'addSqlErrorCallback'});
+	$self->downloadVersion($parameters->{'downloadVersion'});
+
+	$self->init();
+	return $self;
+}
+
+sub init {
+	my $self = shift;
+		my %parserParameters = (
+			'pluginId' => $self->pluginId,
+			'pluginVersion' => $self->pluginVersion,
+			'logHandler' => $self->logHandler,
+			'utf8filenames' => $prefs->get('utf8filenames')
+		);
+		$parserParameters{'cacheName'} = "FileCache/MultiLibrary".$self->pluginVersion."/Templates";
+		$self->templateParser(Plugins::MultiLibrary::ConfigManager::TemplateParser->new(\%parserParameters));
+		$parserParameters{'cacheName'} = "FileCache/MultiLibrary".$self->pluginVersion."/Libraries";
+		$self->contentParser(Plugins::MultiLibrary::ConfigManager::ContentParser->new(\%parserParameters));
+
+		my %parameters = (
+			'logHandler' => $self->logHandler,
+			'criticalErrorCallback' => $self->addSqlErrorCallback,
+			'parameterPrefix' => 'itemparameter'
+		);
+		$self->parameterHandler(Plugins::MultiLibrary::ConfigManager::ParameterHandler->new(\%parameters));
+
+		my %directoryHandlerParameters = (
+			'logHandler' => $self->logHandler,
+			'cacheName' => "FileCache/MultiLibrary/".$self->pluginVersion."/Files",
+		);
+		$directoryHandlerParameters{'extension'} = "ml.xml";
+		$directoryHandlerParameters{'parser'} = $self->contentParser;
+		$directoryHandlerParameters{'includeExtensionInIdentifier'} = undef;
+		$self->contentDirectoryHandler(Plugins::MultiLibrary::ConfigManager::DirectoryLoader->new(\%directoryHandlerParameters));
+
+		$directoryHandlerParameters{'extension'} = "xml";
+		$directoryHandlerParameters{'identifierExtension'} = "xml";
+		$directoryHandlerParameters{'parser'} = $self->templateParser;
+		$directoryHandlerParameters{'includeExtensionInIdentifier'} = 1;
+		$self->templateDirectoryHandler(Plugins::MultiLibrary::ConfigManager::DirectoryLoader->new(\%directoryHandlerParameters));
+
+		$directoryHandlerParameters{'extension'} = "template";
+		$directoryHandlerParameters{'identifierExtension'} = "xml";
+		$directoryHandlerParameters{'parser'} = $self->contentParser;
+		$directoryHandlerParameters{'includeExtensionInIdentifier'} = 1;
+		$self->templateDataDirectoryHandler(Plugins::MultiLibrary::ConfigManager::DirectoryLoader->new(\%directoryHandlerParameters));
+
+		my %pluginHandlerParameters = (
+			'logHandler' => $self->logHandler,
+			'pluginId' => $self->pluginId,
+		);
+
+		$pluginHandlerParameters{'listMethod'} = "getMultiLibraryTemplates";
+		$pluginHandlerParameters{'dataMethod'} = "getMultiLibraryTemplateData";
+		$pluginHandlerParameters{'contentType'} = "template";
+		$pluginHandlerParameters{'contentParser'} = $self->templateParser;
+		$pluginHandlerParameters{'templateContentParser'} = undef;
+		$self->templatePluginHandler(Plugins::MultiLibrary::ConfigManager::PluginLoader->new(\%pluginHandlerParameters));
+
+		$parserParameters{'templatePluginHandler'} = $self->templatePluginHandler;
+		$parserParameters{'cacheName'} = "FileCache/MultiLibrary".$self->pluginVersion."/Libraries";
+		$self->templateContentParser(Plugins::MultiLibrary::ConfigManager::TemplateContentParser->new(\%parserParameters));
+
+		$directoryHandlerParameters{'extension'} = "ml.values.xml";
+		$directoryHandlerParameters{'parser'} = $self->templateContentParser;
+		$directoryHandlerParameters{'includeExtensionInIdentifier'} = undef;
+		$self->templateContentDirectoryHandler(Plugins::MultiLibrary::ConfigManager::DirectoryLoader->new(\%directoryHandlerParameters));
+
+		$pluginHandlerParameters{'listMethod'} = "getMultiLibraryLibraries";
+		$pluginHandlerParameters{'dataMethod'} = "getMultiLibraryLibraryData";
+		$pluginHandlerParameters{'contentType'} = "library";
+		$pluginHandlerParameters{'contentParser'} = $self->contentParser;
+		$pluginHandlerParameters{'templateContentParser'} = $self->templateContentParser;
+		$self->contentPluginHandler(Plugins::MultiLibrary::ConfigManager::PluginLoader->new(\%pluginHandlerParameters));
+
+		$self->initWebAdminMethods();
+}
+sub initWebAdminMethods {
+	my $self = shift;
+
+	my %webTemplates = (
+		'webEditItems' => 'plugins/MultiLibrary/multilibrary_list.html',
+		'webEditItem' => 'plugins/MultiLibrary/webadminmethods_edititem.html',
+		'webEditSimpleItem' => 'plugins/MultiLibrary/webadminmethods_editsimpleitem.html',
+		'webNewItem' => 'plugins/MultiLibrary/webadminmethods_newitem.html',
+		'webNewSimpleItem' => 'plugins/MultiLibrary/webadminmethods_newsimpleitem.html',
+		'webNewItemParameters' => 'plugins/MultiLibrary/webadminmethods_newitemparameters.html',
+		'webNewItemTypes' => 'plugins/MultiLibrary/webadminmethods_newitemtypes.html',
+		'webDownloadItem' => 'plugins/MultiLibrary/webadminmethods_downloaditem.html',
+		'webSaveDownloadedItem' => 'plugins/MultiLibrary/webadminmethods_savedownloadeditem.html',
+		'webPublishLogin' => 'plugins/MultiLibrary/webadminmethods_login.html',
+		'webPublishRegister' => 'plugins/MultiLibrary/webadminmethods_register.html',
+		'webPublishItemParameters' => 'plugins/MultiLibrary/webadminmethods_publishitemparameters.html',
+	);
+
+	my @itemDirectories = ();
+	my @templateDirectories = ();
+	my $dir = $prefs->get("library_directory");
+	if (defined $dir && -d $dir) {
+		push @itemDirectories,$dir
+	}
+	$dir = $prefs->get("template_directory");
+	if (defined $dir && -d $dir) {
+		push @templateDirectories,$dir
+	}
+	my $internalSupportDownloadError = undef;
+	if(!defined($dir) || !-d $dir) {
+		$internalSupportDownloadError = 'You have to specify a template directory before you can download library templates';
+	}
+	if(defined($self->supportDownloadError)) {
+		$internalSupportDownloadError = $self->supportDownloadError;
+	}
+	my @pluginDirs = Slim::Utils::OSDetect::dirsFor('Plugins');
+	for my $plugindir (@pluginDirs) {
+		if( -d catdir($plugindir,"MultiLibrary","Libraries")) {
+			push @itemDirectories, catdir($plugindir,"MultiLibrary","Libraries")
+		}
+		if( -d catdir($plugindir,"MultiLibrary","Templates")) {
+			push @templateDirectories, catdir($plugindir,"MultiLibrary","Templates")
+		}
+	}
+	my %webAdminMethodsParameters = (
+		'pluginId' => $self->pluginId,
+		'pluginVersion' => $self->pluginVersion,
+		'pluginPrefs' => $self->pluginPrefs,
+		'downloadApplicationId' => $self->downloadApplicationId,
+		'extension' => 'ml.xml',
+		'simpleExtension' => 'ml.values.xml',
+		'logHandler' => $self->logHandler,
+		'contentPluginHandler' => $self->contentPluginHandler,
+		'templatePluginHandler' => $self->templatePluginHandler,
+		'contentDirectoryHandler' => $self->contentDirectoryHandler,
+		'contentTemplateDirectoryHandler' => $self->templateContentDirectoryHandler,
+		'templateDirectoryHandler' => $self->templateDirectoryHandler,
+		'templateDataDirectoryHandler' => $self->templateDataDirectoryHandler,
+		'parameterHandler' => $self->parameterHandler,
+		'contentParser' => $self->contentParser,
+		'templateDirectories' => \@templateDirectories,
+		'itemDirectories' => \@itemDirectories,
+		'customTemplateDirectory' => $prefs->get("template_directory"),
+		'customItemDirectory' => $prefs->get("library_directory"),
+		'supportDownload' => 1,
+		'supportDownloadError' => $internalSupportDownloadError,
+		'webCallbacks' => $self,
+		'webTemplates' => \%webTemplates,
+		'downloadUrl' => $prefs->get("download_url"),
+		'utf8filenames' => $prefs->get('utf8filenames'),
+		'downloadVersion' => $self->downloadVersion,
+	);
+	$self->webAdminMethods(Plugins::MultiLibrary::ConfigManager::LibraryWebAdminMethods->new(\%webAdminMethodsParameters));
+
+}
+
+sub readTemplateConfiguration {
+	my $self = shift;
+	my $client = shift;
+	
+	my %templates = ();
+	my %globalcontext = ();
+	my @pluginDirs = Slim::Utils::OSDetect::dirsFor('Plugins');
+	for my $plugindir (@pluginDirs) {
+		$self->logHandler->debug("Checking for dir: ".catdir($plugindir,"MultiLibrary","Templates")."\n");
+		next unless -d catdir($plugindir,"MultiLibrary","Templates");
+		$globalcontext{'source'} = 'builtin';
+		$self->templateDirectoryHandler()->readFromDir($client,catdir($plugindir,"MultiLibrary","Templates"),\%templates,\%globalcontext);
+	}
+
+	$globalcontext{'source'} = 'plugin';
+	$self->templatePluginHandler()->readFromPlugins($client,\%templates,undef,\%globalcontext);
+
+	my $templateDir = $prefs->get('template_directory');
+	$self->logHandler->debug("Checking for dir: $templateDir\n");
+	if($templateDir && -d $templateDir) {
+		$globalcontext{'source'} = 'custom';
+		$self->templateDirectoryHandler()->readFromDir($client,$templateDir,\%templates,\%globalcontext);
+	}
+	return \%templates;
+}
+
+sub readItemConfiguration {
+	my $self = shift;
+	my $client = shift;
+	my $storeInCache = shift;
+	
+	my $dir = $prefs->get("library_directory");
+    	$self->logHandler->debug("Searching for item configuration in: $dir\n");
+    
+	my %localItems = ();
+
+	my @pluginDirs = Slim::Utils::OSDetect::dirsFor('Plugins');
+
+	my %globalcontext = ();
+	$self->templates($self->readTemplateConfiguration());
+
+	$globalcontext{'source'} = 'plugin';
+	$globalcontext{'templates'} = $self->templates;
+
+	$self->contentPluginHandler->readFromPlugins($client,\%localItems,undef,\%globalcontext);
+	for my $plugindir (@pluginDirs) {
+		$globalcontext{'source'} = 'builtin';
+		$self->logHandler->debug("Checking for dir: ".catdir($plugindir,"MultiLibrary","Menus")."\n");
+		if( -d catdir($plugindir,"MultiLibrary","Libraries")) {
+			$self->contentDirectoryHandler()->readFromDir($client,catdir($plugindir,"MultiLibrary","Libraries"),\%localItems,\%globalcontext);
+			$self->templateContentDirectoryHandler()->readFromDir($client,catdir($plugindir,"MultiLibrary","Libraries"),\%localItems, \%globalcontext);
+		}
+	}
+	$self->logHandler->debug("Checking for dir: $dir\n");
+	if (!defined $dir || !-d $dir) {
+		$self->logHandler->debug("Skipping custom browse configuration scan - directory is undefined\n");
+	}else {
+		$globalcontext{'source'} = 'custom';
+		$self->contentDirectoryHandler()->readFromDir($client,$dir,\%localItems,\%globalcontext);
+		$self->templateContentDirectoryHandler()->readFromDir($client,$dir,\%localItems, \%globalcontext);
+	}
+
+	for my $key (keys %localItems) {
+		postProcessItem($localItems{$key});
+	}
+	if($storeInCache) {
+		$self->items(\%localItems);
+	}
+
+	my %result = (
+		'libraries' => \%localItems,
+		'templates' => $self->templates
+	);
+	return \%result;
+}
+sub postProcessItem {
+	my $item = shift;
+	
+	if(defined($item->{'name'})) {
+		$item->{'name'} =~ s/\\\\/\\/g;
+		$item->{'name'} =~ s/\\\"/\"/g;
+		$item->{'name'} =~ s/\\\'/\'/g;
+	}
+}
+
+sub changedItemConfiguration {
+        my ($self, $client, $params) = @_;
+	Plugins::MultiLibrary::Plugin::initLibraries($client);
+	if($prefs->get("refresh_save")) {
+		Plugins::MultiLibrary::Plugin::refreshLibraries();
+		if(UNIVERSAL::can("Plugins::CustomBrowse::Plugin","readBrowseConfiguration")) {
+			no strict 'refs';
+			eval { &{"Plugins::CustomBrowse::Plugin::readBrowseConfiguration"}($client) };
+			use strict 'refs';
+		}
+	}
+}
+
+sub changedTemplateConfiguration {
+        my ($self, $client, $params) = @_;
+	$self->readTemplateConfiguration($client);
+}
+
+sub webEditItems {
+        my ($self, $client, $params) = @_;
+	return Plugins::MultiLibrary::Plugin::handleWebList($client,$params);
+}
+
+sub webEditItem {
+        my ($self, $client, $params) = @_;
+
+	if(!defined($self->items)) {
+		my $itemConfiguration = $self->readItemConfiguration($client);
+		$self->items($itemConfiguration->{'libraries'});
+	}
+	if(!defined($self->templates)) {
+		$self->templates($self->readTemplateConfiguration($client));
+	}
+	
+	return $self->webAdminMethods->webEditItem($client,$params,$params->{'item'},$self->items,$self->templates);	
+}
+
+
+sub webDeleteItemType {
+        my ($self, $client, $params) = @_;
+	return $self->webAdminMethods->webDeleteItemType($client,$params,$params->{'itemtemplate'});	
+}
+
+
+sub webNewItemTypes {
+        my ($self, $client, $params) = @_;
+
+	$self->templates($self->readTemplateConfiguration($client));
+	
+	return $self->webAdminMethods->webNewItemTypes($client,$params,$self->templates);	
+}
+
+sub webNewItemParameters {
+        my ($self, $client, $params) = @_;
+
+	if(!defined($self->templates) || !defined($self->templates->{$params->{'itemtemplate'}})) {
+		$self->templates($self->readTemplateConfiguration($client));
+	}
+	return $self->webAdminMethods->webNewItemParameters($client,$params,$params->{'itemtemplate'},$self->templates);	
+}
+
+sub webLogin {
+        my ($self, $client, $params) = @_;
+
+	return $self->webAdminMethods->webPublishLogin($client,$params,$params->{'item'});	
+}
+
+
+sub webPublishItemParameters {
+        my ($self, $client, $params) = @_;
+
+	if(!defined($self->templates)) {
+		$self->templates($self->readTemplateConfiguration($client));
+	}
+	if(!defined($self->items)) {
+		my $itemConfiguration = $self->readItemConfiguration($client);
+		$self->items($itemConfiguration->{'libraries'});
+	}
+
+	return $self->webAdminMethods->webPublishItemParameters($client,$params,$params->{'item'},$self->items->{$params->{'item'}}->{'name'},$self->items,$self->templates);	
+}
+
+sub webPublishItem {
+        my ($self, $client, $params) = @_;
+
+	if(!defined($self->templates)) {
+		$self->templates($self->readTemplateConfiguration($client));
+	}
+	if(!defined($self->items)) {
+		my $itemConfiguration = $self->readItemConfiguration($client);
+		$self->items($itemConfiguration->{'libraries'});
+	}
+
+	return $self->webAdminMethods->webPublishItem($client,$params,$params->{'item'},$self->items,$self->templates);	
+}
+
+sub webDownloadItems {
+        my ($self, $client, $params) = @_;
+	return $self->webAdminMethods->webDownloadItems($client,$params);	
+}
+
+sub webDownloadNewItems {
+        my ($self, $client, $params) = @_;
+
+	if(!defined($self->templates)) {
+		$self->templates($self->readTemplateConfiguration($client));
+	}
+
+	return $self->webAdminMethods->webDownloadNewItems($client,$params,$self->templates);	
+}
+
+sub webDownloadItem {
+        my ($self, $client, $params) = @_;
+	return $self->webAdminMethods->webDownloadItem($client,$params,$params->{'itemtemplate'});	
+}
+
+sub webNewItem {
+        my ($self, $client, $params) = @_;
+
+	if(!defined($self->templates)) {
+		$self->templates($self->readTemplateConfiguration($client));
+	}
+	
+	return $self->webAdminMethods->webNewItem($client,$params,$params->{'itemtemplate'},$self->templates);	
+}
+
+sub webSaveSimpleItem {
+        my ($self, $client, $params) = @_;
+
+	if(!defined($self->templates)) {
+		$self->templates($self->readTemplateConfiguration($client));
+	}
+	$params->{'items'} = $self->items;
+	
+	return $self->webAdminMethods->webSaveSimpleItem($client,$params,$params->{'itemtemplate'},$self->templates);	
+}
+
+sub webRemoveItem {
+        my ($self, $client, $params) = @_;
+
+	if(!defined($self->items)) {
+		my $itemConfiguration = $self->readItemConfiguration($client);
+		$self->items($itemConfiguration->{'libraries'});
+	}
+	return $self->webAdminMethods->webDeleteItem($client,$params,$params->{'item'},$self->items);	
+}
+
+sub webSaveNewSimpleItem {
+        my ($self, $client, $params) = @_;
+
+	if(!defined($self->templates)) {
+		$self->templates($self->readTemplateConfiguration($client));
+	}
+	$params->{'items'} = $self->items;
+	
+	return $self->webAdminMethods->webSaveNewSimpleItem($client,$params,$params->{'itemtemplate'},$self->templates);	
+}
+
+sub webSaveNewItem {
+        my ($self, $client, $params) = @_;
+	$params->{'items'} = $self->items;
+	return $self->webAdminMethods->webSaveNewItem($client,$params);	
+}
+
+sub webSaveItem {
+        my ($self, $client, $params) = @_;
+	$params->{'items'} = $self->items;
+	return $self->webAdminMethods->webSaveItem($client,$params);	
+}
+
+1;
+
+__END__
