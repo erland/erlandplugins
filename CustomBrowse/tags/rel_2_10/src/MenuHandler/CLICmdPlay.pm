f864a6afb55b468f97dd0b437894f034f277468c
isaksson.erland <isaksson.erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2009-10-29 06:58:22 +0000

    Creating release 2.10

---
+++ CustomBrowse/tags/rel_2_10/src/MenuHandler/CLICmdPlay.pm
@@ -0,0 +1,76 @@
+# 			MenuHandler::FunctionCmdPlay module
+#
+#    Copyright (c) 2006 Erland Isaksson (erland_i@hotmail.com)
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package Plugins::CustomBrowse::MenuHandler::CLICmdPlay;
+
+use strict;
+
+use base qw(Slim::Utils::Accessor);
+use Plugins::CustomBrowse::MenuHandler::BasePlay;
+our @ISA = qw(Plugins::CustomBrowse::MenuHandler::BasePlay);
+
+use File::Spec::Functions qw(:ALL);
+
+__PACKAGE__->mk_accessor( rw => qw(itemParameterHandler requestSource) );
+
+sub new {
+	my $class = shift;
+	my $parameters = shift;
+
+	my $self = $class->SUPER::new($parameters);
+	$self->itemParameterHandler($parameters->{'itemParameterHandler'});
+	$self->requestSource($parameters->{'requestSource'});
+
+	return $self;
+}
+
+sub implementsPlay {
+
+	return 1;
+}
+
+sub play {
+	my $self = shift;
+	my $client = shift;
+	my $item = shift;
+	my $items = shift;
+	my $cmd = shift;
+
+	my $result = undef;
+	if(defined($item->{'playdata'})) {
+		my @cmds = split(/\|/,$item->{'playdata'});
+		for my $cmd (@cmds) {
+			my $keywords = $self->combineKeywords($item->{'keywordparameters'},undef,$item->{'parameters'});
+			$cmd=$self->itemParameterHandler->replaceParameters($client,$cmd,$keywords);
+			my @cmdParts = split(/ /,$cmd);
+			my $request = $client->execute(\@cmdParts);
+			if(defined($request)) {
+				$request->source($self->requestSource);
+			}else {
+				$self->errorCallback->("CustomBrowse: ERROR, couldn't execute CLI command $cmd\n");
+			}
+		}
+	}else {
+		$self->errorCallback->("CustomBrowse: ERROR, no playdata element found\n");
+	}
+	return 0;
+}
+
+1;
+
+__END__
---
+++ CustomBrowse/tags/rel_2_10/src/MenuHandler/ParameterHandler.pm
@@ -0,0 +1,142 @@
+# 			ParameterHandler module
+#
+#    Copyright (c) 2006 Erland Isaksson (erland_i@hotmail.com)
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package Plugins::CustomBrowse::MenuHandler::ParameterHandler;
+
+use strict;
+
+use base qw(Slim::Utils::Accessor);
+
+use File::Spec::Functions qw(:ALL);
+use Slim::Utils::Prefs;
+
+__PACKAGE__->mk_accessor( rw => qw(logHandler pluginId pluginVersion propertyHandler) );
+
+my %prefs = ();
+
+sub new {
+	my $class = shift;
+	my $parameters = shift;
+
+	my $self = $class->SUPER::new();
+	$self->logHandler($parameters->{'logHandler'});
+	$self->pluginId($parameters->{'pluginId'});
+	$self->pluginVersion($parameters->{'pluginVersion'});
+	$self->propertyHandler($parameters->{'propertyHandler'});
+
+	return $self;
+}
+
+sub quoteValue {
+	my $value = shift;
+	$value =~ s/\\/\\\\/g;
+	$value =~ s/\'/\\\'/g;
+	$value =~ s/\"/\\\"/g;
+	return $value;
+}
+
+sub replaceParameters {
+	my $self = shift;
+	my $client = shift;
+	my $originalValue = shift;
+	my $parameters = shift;
+	my $context = shift;
+	my $quote = shift;
+
+	if(defined($parameters)) {
+		for my $param (keys %$parameters) {
+			my $propertyValue = $parameters->{$param};
+			if($quote) {
+				$propertyValue=quoteValue($propertyValue);
+			}
+			$originalValue =~ s/\{$param\}/$propertyValue/g;
+		}
+	}
+	while($originalValue =~ m/\{custombrowse\.(.*?)\}/) {
+		my $propertyValue = $self->propertyHandler->getProperty($1);
+		if(defined($propertyValue)) {
+			if($quote) {
+				$propertyValue=quoteValue($propertyValue);
+			}
+			$originalValue =~ s/\{custombrowse\.$1\}/$propertyValue/g;
+		}else {
+			$originalValue =~ s/\{custombrowse\..*?\}//g;
+		}
+	}
+	while($originalValue =~ m/\{property:(.*?):(.*?)\}/) {
+		my $propContext = $1;
+		my $propName = $2;
+		if(!defined($prefs{$propContext})) {
+			$prefs{$propContext}=preferences($propContext);
+		}
+		my $propertyValue = $prefs{$propContext}->get($propName);
+		if(defined($propertyValue)) {
+			if($quote) {
+				$propertyValue=quoteValue($propertyValue);
+			}
+			$originalValue =~ s/\{property:$propContext:$propName\}/$propertyValue/g;
+		}else {
+			$originalValue =~ s/\{property:$propContext:$propName\}//g;
+		}
+	}
+	while($originalValue =~ m/\{clientproperty:(.*?):(.*?)\}/) {
+		my $propContext = $1;
+		my $propName = $2;
+		if(!defined($prefs{$propContext})) {
+			$prefs{$propContext}=preferences($propContext);
+		}
+		my $propertyValue = undef;
+		if(defined($client)) {
+			$propertyValue = $prefs{$propContext}->client($client)->get($propName);
+		}
+		if(defined($propertyValue)) {
+			if($quote) {
+				$propertyValue=quoteValue($propertyValue);
+			}
+			$originalValue =~ s/\{clientproperty:$propContext:$propName\}/$propertyValue/g;
+		}else {
+			$originalValue =~ s/\{clientproperty:$propContext:$propName\}//g;
+		}
+	}
+	while($originalValue =~ m/\{context\.(.*?)\}/) {
+		my $propertyValue = undef;
+		my $contextHash = $context;
+		if(!defined($contextHash)) {
+			$contextHash = $client->modeParam($self->pluginId.".context");
+		}
+		if(defined($contextHash)) {
+			$propertyValue = $contextHash->{$1};
+			#$propertyValue = Slim::Utils::Unicode::utf8on($propertyValue);
+			#$propertyValue = Slim::Utils::Unicode::utf8encode_locale($propertyValue);
+		}
+		if(defined($propertyValue)) {
+			if($quote) {
+				$propertyValue=quoteValue($propertyValue);
+			}
+			$originalValue =~ s/\{context\.$1\}/$propertyValue/g;
+		}else {
+			$originalValue =~ s/\{context\..*?\}//g;
+		}
+	}
+
+	return $originalValue;
+}
+
+1;
+
+__END__
