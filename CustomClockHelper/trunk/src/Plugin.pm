cb0e790278133bd9648b6ed0c90e56093f58f36f
isaksson.erland <isaksson.erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2010-05-01 13:54:17 +0000

    Changed so clockstyles4.json is used

--- CustomClockHelper/trunk/src/Plugin.pm
+++ CustomClockHelper/trunk/src/Plugin.pm
@@ -141,7 +141,7 @@ sub getStyles {
 	my $styles = {};
 	if(!$localOnly) {
 		my $http = LWP::UserAgent->new;
-		my $response = $http->get("http://erlandplugins.googlecode.com/svn/CustomClock/trunk/clockstyles3.json");
+		my $response = $http->get("http://erlandplugins.googlecode.com/svn/CustomClock/trunk/clockstyles4.json");
 		if($response->is_success) {
 			my $jsonStyles = $response->content;
 			eval {
