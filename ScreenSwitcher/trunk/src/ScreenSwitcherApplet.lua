50f94c6ef7c7f3b6b5af36f465cb28b7703002d2
isaksson.erland <isaksson.erland@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2010-05-16 08:51:01 +0000

    Corrected so random mode works with image viewer screen savers, added support for enabling screens during specific weekdays or times

--- ScreenSwitcher/trunk/src/ScreenSwitcherApplet.lua
+++ ScreenSwitcher/trunk/src/ScreenSwitcherApplet.lua
@@ -19,7 +19,7 @@ following methods:
 
 
 -- stuff we use
-local pairs, ipairs, tostring, tonumber, pcall = pairs, ipairs, tostring, tonumber, pcall
+local pairs, ipairs, tostring, tonumber, setmetatable, type, pcall = pairs, ipairs, tostring, tonumber, setmetatable, type, pcall
 
 local oo               = require("loop.simple")
 local os               = require("os")
@@ -38,10 +38,14 @@ local SimpleMenu       = require("jive.ui.SimpleMenu")
 local RadioGroup       = require("jive.ui.RadioGroup")
 local RadioButton      = require("jive.ui.RadioButton")
 local Timer            = require("jive.ui.Timer")
+local Checkbox         = require("jive.ui.Checkbox")
+local Timeinput         = require("jive.ui.Timeinput")
+local DateTime               = require("jive.utils.datetime")
 
 local appletManager    = appletManager
 local jiveMain         = jiveMain
 local jnt              = jnt
+local jive             = jive
 
 local WH_FILL           = jive.ui.WH_FILL
 local LAYOUT_NORTH      = jive.ui.LAYOUT_NORTH
@@ -167,7 +171,7 @@ function _updateScreensaver(self, forced)
 	end
 	if ss then
 		log:debug("Start new timer, trigger after "..ss.delay.." seconds")
-		self.timer = Timer(ss.delay*1000, function() 
+		self.timer = Timer(5*1000, function() 
 			local screensaversApplet = appletManager:loadApplet("ScreenSavers") 
 			local activeWindows = screensaversApplet["active"]
 			if #activeWindows>0 then
@@ -188,54 +192,190 @@ function isSoftPowerOn(self)
         return jiveMain:getSoftPowerState() == "on"
 end
 
+function _uses(parent, value)
+        if parent == nil then
+                log:warn("nil parent in _uses")
+        end
+        local style = {}
+        setmetatable(style, { __index = parent })
+        for k,v in pairs(value or {}) do
+                if type(v) == "table" and type(parent[k]) == "table" then
+                        -- recursively inherrit from parent style
+                        style[k] = _uses(parent[k], v)
+                else
+                        style[k] = v
+                end
+        end
+
+        return style
+end
+
+function _getDateTimeSelectionString(self,state)
+	local name = ""
+	local starttime = self:getSettings()[state.."starttime"] or 0
+	starttime = tonumber(starttime)
+	local endtime = self:getSettings()[state.."endtime"] or 0
+	endtime = tonumber(endtime)
+
+	if self:getSettings()[state.."enabled"] then
+		if starttime != 0 or endtime != 0 then
+			if starttime != 0 then
+				local time = DateTime:timeTableFromSFM(starttime, '24')
+				if time.minute<10 then
+					name = name..time.hour..":0"..time.minute.."-"
+				else
+					name = name..time.hour..":"..time.minute.."-"
+				end
+			end
+			if endtime != 0 then
+				local time = DateTime:timeTableFromSFM(endtime, '24')
+				if starttime == 0 then
+					name = name.."0:00-"
+				end
+				if time.minute<10 then
+					name = name..time.hour..":0"..time.minute
+				else
+					name = name..time.hour..":"..time.minute
+				end
+			else
+				name = name .. "0:00"
+			end
+		end
+
+		local weekdays = self:getSettings()[state.."weekdays"]
+		if weekdays then
+			local alldays = true
+			for i = 0,6 do
+				if not weekdays[i] then
+					alldays = false
+				end
+			end
+			if not alldays then
+				if name ~= "" then
+					name = name .." "
+				end
+				local first = true
+				for i = 0,6 do
+					if weekdays[i] then
+						if not first then
+							name = name.. ","
+						end
+						name = name .. tostring(self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_WEEKDAYS_SHORT_"..i))
+						first = false
+					end
+				end
+			end
+		end
+		if name == "" then
+			name = tostring(self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_ALWAYS"))
+		end
+	else
+		name = tostring(self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_DISABLED"))
+	end
+	return name
+end
+
 function openSettings(self)
 	log:debug("Screen Switcher settings")
 
-	local window = Window("text_list", self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS"), 'settingstitle')
+	jive.ui.style.item_no_icon = _uses(jive.ui.style.item, {
+		order = { 'text', 'check' },
+	})
+	jive.ui.style.icon_list.menu.item_no_icon = _uses(jive.ui.style.icon_list.menu.item, {
+		order = { 'text', 'check' },
+	})
+	jive.ui.style.icon_list.menu.selected.item_no_icon = _uses(jive.ui.style.icon_list.menu.selected.item, {
+		order = { 'text', 'check' },
+	})
+	jive.ui.style.icon_list.menu.pressed.item_no_icon = _uses(jive.ui.style.icon_list.menu.pressed.item, {
+	})
+	local window = Window("icon_list", self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS"), 'settingstitle')
 
 	local menu = SimpleMenu("menu")
 	window:addWidget(menu)
 
-	menu:addItem(
-		{
-			text = self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_PLAYING"), 
-			sound = "WINDOWSHOW",
-			callback = function(event, menuItem)
-				self:defineSettingScreens(menuItem,"playing")
-				return EVENT_CONSUME
-			end
-		}
-	)
-
-	menu:addItem(
-		{
-			text = self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_STOPPED"), 
-			sound = "WINDOWSHOW",
-			callback = function(event, menuItem)
-				self:defineSettingScreens(menuItem,"stopped")
-				return EVENT_CONSUME
-			end
-		}
-	)
-	if System:getMachine() ~= "jive" then
+	for i = 1,3 do
+		local name = " "
+		local timelimited = true
+		local no = i
+		if i == 3 then
+			name = tostring(self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_DEFAULT"))
+			timelimited = false
+			no = ""
+		else
+			name = name .. self:_getDateTimeSelectionString("playing"..i)
+		end
 		menu:addItem(
 			{
-				text = self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_OFF"), 
+				text = tostring(self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_PLAYING")).." #"..i.."\n"..name, 
+				style = 'item_no_icon',
 				sound = "WINDOWSHOW",
 				callback = function(event, menuItem)
-					self:defineSettingScreens(menuItem,"off")
+					self:defineSettingScreens(menuItem,"playing"..no,timelimited)
 					return EVENT_CONSUME
 				end
 			}
 		)
+	end
+	for i = 1,3 do
+		local name = ""
+		local timelimited = true
+		local no = i
+		if i == 3 then
+			name = tostring(self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_DEFAULT"))
+			timelimited = false
+			no = ""
+		else
+			name = name .. self:_getDateTimeSelectionString("stopped"..i)
+		end
+		menu:addItem(
+			{
+				text = tostring(self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_STOPPED")).." #"..i.."\n"..name, 
+				style = 'item_no_icon',
+				sound = "WINDOWSHOW",
+				callback = function(event, menuItem)
+					self:defineSettingScreens(menuItem,"stopped"..no,timelimited)
+					return EVENT_CONSUME
+				end
+			}
+		)
+	end
+	if System:getMachine() ~= "jive" then
+		for i = 1,3 do
+			local name = ""
+			local timelimited = true
+			local no = i
+			if i == 3 then
+				name = tostring(self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_DEFAULT"))
+				timelimited = false
+				no = ""
+			else
+				name = name .. self:_getDateTimeSelectionString("off"..i)
+			end
+			menu:addItem(
+				{
+					text = tostring(self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_OFF")).." #"..i.."\n"..name, 
+					style = 'item_no_icon',
+					sound = "WINDOWSHOW",
+					callback = function(event, menuItem)
+						self:defineSettingScreens(menuItem,"off"..no,timelimited)
+						return EVENT_CONSUME
+					end
+				}
+			)
+		end
 	end
 
 	self:tieAndShowWindow(window)
 	return window
 end
 
-function defineSettingScreens(self, menuItem, state)
+function defineSettingScreens(self, menuItem, state,timelimited)
 	local screens = self:getSettings()[state.."screens"]
+	if not screens then
+		self:getSettings()[state.."screens"] = {}
+		screens = {}
+	end
 
 	local window = Window("text_list", menuItem.text, 'settingstitle')
 
@@ -244,6 +384,45 @@ function defineSettingScreens(self, menuItem, state)
 
 	local screensaversApplet = appletManager:loadApplet("ScreenSavers")
 	local screensavers = screensaversApplet["screensavers"]
+
+	if timelimited then
+		menu:addItem({
+			text = self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_ENABLE"), 
+			style = 'item_choice',
+			check = Checkbox(
+				"checkbox",
+				function(object, isSelected)
+					self:getSettings()[state.."enabled"] = isSelected
+					self:storeSettings()
+				end,
+				self:getSettings()[state.."enabled"]
+			)
+                })
+		menu:addItem({
+			text = tostring(self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_WEEKDAYS")),
+			sound = "WINDOWSHOW",
+			callback = function(event, menuItem)
+				self:defineSettingWeekdays(menuItem,state)
+				return EVENT_CONSUME
+			end
+		})
+		menu:addItem({
+			text = tostring(self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_STARTTIME")),
+			sound = "WINDOWSHOW",
+			callback = function(event, menuItem)
+				self:defineSettingTime(menuItem,state.."starttime",0)
+				return EVENT_CONSUME
+			end
+		})
+		menu:addItem({
+			text = tostring(self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_ENDTIME")),
+			sound = "WINDOWSHOW",
+			callback = function(event, menuItem)
+				self:defineSettingTime(menuItem,state.."endtime",0)
+				return EVENT_CONSUME
+			end
+		})
+	end
 
 	local index = 0
 	for _,screen in pairs(screens) do
@@ -275,6 +454,85 @@ function defineSettingScreens(self, menuItem, state)
 			})
 			index = index+1
 		end
+	end
+
+	self:tieAndShowWindow(window)
+	return window
+end
+
+local function _getTimeFormat()
+        local SetupDateTimeSettings = appletManager:callService("setupDateTimeSettings")
+        local format = '12'
+        if SetupDateTimeSettings and SetupDateTimeSettings['hours'] then
+                format = SetupDateTimeSettings['hours']
+        end
+        return format
+end
+
+function defineSettingTime(self, menuItem, timeattribute,default,seconds)
+	local time = self:getSettings()[timeattribute] or default
+	local window = nil
+	local timeStruct = nil
+--	if _getTimeFormat() == "12" then
+		window = Window("input_time_12h", menuItem.text, 'settingstitle')
+		timeStruct = DateTime:timeTableFromSFM(tonumber(time), '12')
+--	else
+--		window = Window("input_time_24h", menuItem.text, 'settingstitle')
+--		timeStruct = DateTime:timeTableFromSFM(tonumber(time), '24')
+--	end
+        local input = Timeinput(window, 
+                                function(hour, minute, ampm)
+					if ampm then
+						local time = tonumber(hour)*3600+tonumber(minute)*60
+						if ampm == "AM" and tonumber(hour) == 12 then
+		                                        time = tonumber(minute)*60
+						elseif ampm == "PM" and tonumber(hour)<12 then
+		                                        time = time + 43200
+						end
+						self:getSettings()[timeattribute] = time
+					else
+	                                        self:getSettings()[timeattribute] = tonumber(hour)*3600+tonumber(minute*60)
+					end
+					self:storeSettings()
+                                end,
+				timeStruct)
+
+	self:tieAndShowWindow(window)
+	return window
+end
+
+function defineSettingWeekdays(self, menuItem, state)
+	local weekdays = self:getSettings()[state.."weekdays"]
+	if not weekdays then
+		weekdays = {}
+		for i = 0,6 do
+			weekdays[i] = true
+		end
+	end
+	local window = Window("text_list", menuItem.text, 'settingstitle')
+
+	local menu = SimpleMenu("menu")
+	window:addWidget(menu)
+
+	local availableWeekdays = {}
+	for i = 0,6 do
+		availableWeekdays[i] = self:string("SCREENSAVER_SCREENSWITCHER_SETTINGS_WEEKDAYS_"..i)
+	end
+
+	for dayno,daytext in pairs(availableWeekdays) do
+		menu:addItem({
+			text = daytext,
+			style = 'item_choice',
+			check = Checkbox(
+				"checkbox",
+				function(object, isSelected) 
+					weekdays[tonumber(dayno)] = isSelected
+					self:getSettings()[state.."weekdays"] = weekdays
+					self:storeSettings()
+				end,
+				weekdays[tonumber(dayno)]
+			)
+		})
 	end
 
 	self:tieAndShowWindow(window)
@@ -331,7 +589,7 @@ function defineSettingScreensaver(self,menuItem,state,index)
 	local screensaversApplet = appletManager:loadApplet("ScreenSavers")
 	local screensavers = screensaversApplet["screensavers"]
 
-	for _,screensaver in pairs(screensavers) do
+	for key,screensaver in pairs(screensavers) do
 		if screensaver.applet ~= "ScreenSwitcher" then
 			if screensaver.applet then
 				menu:addItem({
@@ -347,12 +605,12 @@ function defineSettingScreensaver(self,menuItem,state,index)
 								delay = screen.delay
 							end
 							self:getSettings()[state.."screens"][index] = {
-								screensaver = self:getKey(screensaver.applet,screensaver.method,screensaver.additionalKey),
+								screensaver = key,
 								delay = delay
 							}
 							self:storeSettings()
 						end,
-						screen and screen.screensaver == self:getKey(screensaver.applet,screensaver.method,screensaver.additionalKey)
+						screen and screen.screensaver == key
 					),
 				})
 			else
@@ -415,9 +673,39 @@ function defineSettingDelay(self,menuItem,state,index)
 	return window
 end
 
+function _getConfiguration(self, state)
+	local weekday = os.date("%w")
+	local now = os.time()
+	local midnight = os.date("*t")
+	midnight.hour = 0
+	midnight.min = 0
+	midnight.sec = 0
+	local secFromMidnight = now - os.time(midnight)
+
+	for i = 1,2 do
+		if self:getSettings()[state..i.."enabled"] then
+			local weekdays = self:getSettings()[state..i.."weekdays"]
+			if not weekdays or weekdays[tonumber(weekday)] then
+				local starttime = self:getSettings()[state..i.."starttime"] or 0
+				local endtime = self:getSettings()[state..i.."endtime"] or 0
+				if endtime == 0 then
+					endtime = (24*3600)
+				end
+				endtime = endtime -1
+				if starttime<=endtime and (secFromMidnight>=starttime) and (secFromMidnight<=endtime) then
+					return state..i
+				elseif starttime>endtime and (secFromMidnight>=starttime or secFromMidnight<=endtime) then
+					return state..i
+				end
+			end
+		end
+	end
+	return state
+end
 function _getNextScreensaver(self,state)
-	local screenSaverList = self:getSettings()[state.."screens"]
-	local currentPos = self.currentPosition[state]
+	local config = self:_getConfiguration(state)
+	local screenSaverList = self:getSettings()[config.."screens"] or {}
+	local currentPos = self.currentPosition[config]
 
 	if not currentPos then
 		currentPos = 1
@@ -429,14 +717,14 @@ function _getNextScreensaver(self,state)
 		currentPos = 1
 	end
 
-	self.currentPosition[state] = currentPos
+	self.currentPosition[config] = currentPos
 
 	if screenSaverList and screenSaverList[currentPos] and screenSaverList[currentPos].screensaver then
-		log:debug("Getting "..state.." screensaver from position "..currentPos)
+		log:debug("Getting "..config.." screensaver from position "..currentPos)
 		log:debug("Returning screen saver "..screenSaverList[currentPos].screensaver)
 		return screenSaverList[currentPos]
 	end
-	log:debug("No "..state.." screensaver defined for position "..currentPos..", returning blank screensaver")
+	log:debug("No "..config.." screensaver defined for position "..currentPos..", returning blank screensaver")
 	return nil
 end
 
@@ -446,15 +734,21 @@ function _getRandomScreensaver(self,state)
 
 	local localScreenSaverList = {}
 	local i = 1
-	for _,saver in pairs(screenSaverList) do
-		if state ~= "off" and saver.applet and self:getKey(saver.applet,saver.method,saver.additionalKey) ~= "BlankScreen:openScreensaver" and saver.applet ~= "ScreenSwitcher" then
+	for key,saver in pairs(screenSaverList) do
+		local additionalKey = nil
+		if string.find(key,"^.*:.*:")  then
+			additionalKey = string.gsub(key,"^.*:.*:","")
+		end
+		if not string.find(state,"^off") and saver and saver.applet and self:getKey(saver.applet,saver.method,additionalKey) ~= "BlankScreen:openScreensaver" and saver.applet ~= "ScreenSwitcher" then
 			localScreenSaverList[i] = saver
+			localScreenSaverList[i].additionalKey = additionalKey
 			i = i+1
-		elseif state == "off" and saver.applet and self:getKey(saver.applet,saver.method,saver.additionalKey) ~= "BlankScreen:openScreensaver" and saver.applet ~= "ScreenSwitcher" and saver.applet ~= "NowPlaying" then
+		elseif string.find(state,"^off") and saver and saver.applet and self:getKey(saver.applet,saver.method,additionalKey) ~= "BlankScreen:openScreensaver" and saver.applet ~= "ScreenSwitcher" and saver.applet ~= "NowPlaying" then
 			localScreenSaverList[i] = saver
+			localScreenSaverList[i].additionalKey = additionalKey
 			i = i+1
 		else 
-			log:debug("Skipping "..state.." saver "..self:getKey(saver.applet,saver.method,saver.additionalKey))
+			log:debug("Skipping "..state.." saver "..self:getKey(saver.applet,saver.method,additionalKey))
 		end
 	end
 	log:debug("Choosing among "..#localScreenSaverList.." screensavers")
