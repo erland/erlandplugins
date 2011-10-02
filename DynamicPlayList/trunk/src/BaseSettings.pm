8052322c7014573f7b38d50b7b04d03121f02735
erland@isaksson.info <erland@isaksson.info@b8993f7f-8a45-0410-8792-83a2c9521e0c>
2011-10-02 07:14:43 +0000

    Added License Manager support

--- DynamicPlayList/trunk/src/BaseSettings.pm
+++ DynamicPlayList/trunk/src/BaseSettings.pm
@@ -63,6 +63,9 @@ sub handler {
 	}
 	$params->{'subpages'} = \%currentSubPages;
 	$params->{'subpage'} = $class->currentPage($client,$params);
+	$params->{'licensemanager'} = Plugins::DynamicPlayList::Plugin::isPluginsInstalled($client,'LicenseManagerPlugin');
+	my $validateRequest = Slim::Control::Request::executeRequest($client,['licensemanager','validate','application:DynamicPlayList']);
+	$params->{'licensed'} = $validateRequest->getResult("result");
 	return $class->SUPER::handler($client, $params);
 }
 		
