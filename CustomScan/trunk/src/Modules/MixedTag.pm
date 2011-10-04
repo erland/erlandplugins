5b4923c269bee0f7df643ad2f649590be18793c7
erland <erland@isaksson.info>
2011-10-04 06:01:07 +0000

    Corrected so it's possible to show artists with albums also in 7.6 with SQLite

--- CustomScan/trunk/src/Modules/MixedTag.pm
+++ CustomScan/trunk/src/Modules/MixedTag.pm
@@ -581,16 +581,16 @@ sub getMixedTagMenuItems {
 			if(defined($parameters->{'roles'}) && $parameters->{'roles'}) {
 				$roles = $parameters->{'roles'};
 			}
-			$albumssqlbyartists = "select albums.id,ifnull(if(albums.compilation,' ',concat('(', group_concat(distinct contributors.name separator ',') ,')')),' '),substr(albums.titlesort,1,1),'album' from albums join tracks on tracks.album=albums.id ";
+			$albumssqlbyartists = "select albums.id,ifnull(if(albums.compilation,' ',concat('(', group_concat(distinct contributors.name) ,')')),' '),substr(albums.titlesort,1,1),'album' from albums join tracks on tracks.album=albums.id ";
 			$albumssqlbyartists .= "left join contributor_track on contributor_track.track=tracks.id and contributor_track.role in ($roles) ";
 			$albumssqlbyartists .= "left join contributors on contributor_track.contributor=contributors.id ";
 
-			if(defined($parameters->{'showartistwithalbum'}) && $parameters->{'showartistwithalbum'} && $driver eq 'mysql') {
-				$albumssql = "select albums.id,ifnull(if(albums.compilation,' ',concat('(', group_concat(distinct contributors.name separator ',') ,')')),' '),substr(albums.titlesort,1,1),'album' from albums join tracks on tracks.album=albums.id ";
+			if(defined($parameters->{'showartistwithalbum'}) && $parameters->{'showartistwithalbum'}) {
+				$albumssql = "select albums.id,ifnull(if(albums.compilation,' ',concat('(', group_concat(distinct contributors.name) ,')')),' '),substr(albums.titlesort,1,1),'album' from albums join tracks on tracks.album=albums.id ";
 				$albumssql .= "left join contributor_track on contributor_track.track=tracks.id and contributor_track.role in ($roles) ";
 				$albumssql .= "left join contributors on contributor_track.contributor=contributors.id ";
 
-				$albumssqlbyyear = "select albums.id,if(albums.compilation,if(albums.year=0,' ',concat('(',albums.year,')')),concat(if(albums.year=0,'(',concat('(',albums.year,' ')), ifnull(group_concat(distinct contributors.name separator ','),'') ,')')),substr(albums.titlesort,1,1),'album' from albums join tracks on tracks.album=albums.id ";
+				$albumssqlbyyear = "select albums.id,if(albums.compilation,if(albums.year=0,' ',concat('(',albums.year,')')),concat(if(albums.year=0,'(',concat('(',albums.year,' ')), ifnull(group_concat(distinct contributors.name),'') ,')')),substr(albums.titlesort,1,1),'album' from albums join tracks on tracks.album=albums.id ";
 				$albumssqlbyyear .= "left join contributor_track on contributor_track.track=tracks.id and contributor_track.role in ($roles) ";
 				$albumssqlbyyear .= "left join contributors on contributor_track.contributor=contributors.id ";
 			}else {
@@ -849,7 +849,7 @@ sub getMixedTagMenuItems {
 			'menudata' => $albumssql,
 			'menu' => \%menutracks
 		);
-		if(defined($parameters->{'showartistwithalbum'}) && $parameters->{'showartistwithalbum'} && $driver eq 'mysql') {
+		if(defined($parameters->{'showartistwithalbum'}) && $parameters->{'showartistwithalbum'}) {
 			$menualbums{'itemformat'} = 'albumconcat';
 		}
 		if(defined($parameters->{'defaultalbumsort'}) && $parameters->{'defaultalbumsort'}) {
@@ -877,9 +877,7 @@ sub getMixedTagMenuItems {
 		my @options = ();
 		push @options, \%menualbumsbytitle;
 		push @options, \%menualbumsbyyear;
-		if($driver eq 'mysql') {
-			push @options, \%menualbumsbyartists;
-		}
+		push @options, \%menualbumsbyartists;
 		$menualbums{'option'} = \@options;
 
 		my %allalbums = (
